5af05cb4b2e90fbcb0b3445d258de5e6f5d31e69
Myles Wirth <regime_skylark.0y@icloud.com>
2025-11-27 00:06:33 -0500

    Modified the core generate function to generate groups and subgroup enum

--- generate/src/gen_lib.rs
+++ generate/src/gen_lib.rs
@@ -1,36 +1,64 @@
 use crate::group_subgroup::Group;
 use crate::sanitize;
+use heck::ToUpperCamelCase;
 use proc_macro2::TokenStream;
 use quote::quote;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::PathBuf;
 use std::process::Command;
 
-pub fn dump(groups: &Vec<Group>, annotation_langs: &Vec<&str>, version: f32, date: String) {
+pub fn dump(
+    groups: &Vec<Group>,
+    group_map: &HashMap<u8, String>,
+    subgroup_map: &HashMap<u8, String>,
+    annotation_langs: &Vec<&str>,
+    version: f32,
+    date: String,
+) {
     let mut fs = String::new();
     let mut f = File::open("generate/src/library_header.rs").unwrap();
     f.read_to_string(&mut fs).unwrap();
     let ts: TokenStream = fs.parse().unwrap();
+
+    // Generate Enums
+    let mut group_variants = vec![];
+    for (_, key) in group_map {
+        let ident =
+            proc_macro2::Ident::new(&key.to_upper_camel_case(), proc_macro2::Span::call_site());
+        group_variants.push(quote! { #ident });
+    }
+
+    let mut subgroup_variants = vec![];
+    for (_, key) in subgroup_map {
+        let ident =
+            proc_macro2::Ident::new(&key.to_upper_camel_case(), proc_macro2::Span::call_site());
+        subgroup_variants.push(quote! { #ident });
+    }
+
     let dump = quote! {
-    #ts
-    /// All annotation languages (feature independent)
-    pub const ANNOTATION_LANGS_TOTAL: &'static [&'static str] = &[
-        #(#annotation_langs),*
-    ];
-    /// Enabled annotation languages (feature dependent)
-    /// Defaults to `["en"]`. Enable the `XX` features for each language to include annotations for another language. For example, to include Finnish annotations, use the `fi` feature.
-    pub const ANNOTATION_LANGS_AVAILABLE: &'static [&'static str] = &[
-        #(#[cfg(feature = #annotation_langs)]#annotation_langs),*
-    ];
-    /// The unicode release version that this crate is compiled against
-    pub const UNICODE_VERSION: f32 = #version;
-    /// The rfc3339 formatted time of the unicode release that this crate is compiled against
-    pub const UNICODE_RELEASE_TIME: &'static str = #date;
-    #(#groups)*
+        #ts
+
+        #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+        pub enum Group {
+            #(#group_variants),*
+        }
+
+        #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+        pub enum Subgroup {
+            #(#subgroup_variants),*
+        }
+
+        /// All annotation languages
+        pub const ANNOTATION_LANGS_TOTAL: &'static [&'static str] = &[
+            #(#annotation_langs),*
+        ];
+        pub const UNICODE_VERSION: f32 = #version;
+        pub const UNICODE_RELEASE_TIME: &'static str = #date;
+        #(#groups)*
     };
 
-    // skeleton; writes the module structure
     let path = "emoji/src/lib.rs";
     let pb: PathBuf = path.into();
     File::create(pb)
@@ -42,6 +70,7 @@ pub fn dump(groups: &Vec<Group>, annotation_langs: &Vec<&str>, version: f32, dat
         .output()
         .expect("Failed to execute command");
 
+    // ... existing subgroup file generation ...
     for g in groups {
         let dir = format!("emoji/src/{}", sanitize(&g.name).to_lowercase());
         let pb: PathBuf = dir.clone().into();
@@ -57,7 +86,7 @@ pub fn dump(groups: &Vec<Group>, annotation_langs: &Vec<&str>, version: f32, dat
             );
             let pb: PathBuf = path.clone().into();
             let dump = quote! {
-            #(#emojis)*
+                #(#emojis)*
             };
             File::create(pb)
                 .unwrap()
